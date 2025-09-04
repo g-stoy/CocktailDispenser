38f9e1260a003b4506d7703f1d90e4fa4f80d5a1
unknown <ge.georgiev95@gmail.com>
2025-09-04 20:23:42 +0300

    Refactor

--- cocktaildispenser/asgi.py
+++ cocktaildispenser/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for cocktailhub project.
+ASGI config for cocktaildispenser project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cocktailhub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cocktaildispenser.settings')
 
 application = get_asgi_application()
